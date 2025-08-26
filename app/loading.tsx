fcf81ec5192feb328418762717b2cb4686db8598
Vadim Cusnir <vadim.kusnir@gmail.com>
2025-08-26 05:43:41 +0300

    feat: clear repo for new implementation - all v3 files removed

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { PromptForgeLoading } from "@/components/loading-states";
-
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <PromptForgeLoading size="lg" message="Initializing Cognitive OS..." />
-    </div>
-  );
-}
